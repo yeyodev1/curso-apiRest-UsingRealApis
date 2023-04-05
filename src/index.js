3e78865dd18cf50d21a12867ec4d24ca85de7983
Diego Reyes <diegorele13@gmail.com>
2023-04-05 16:53:14 -0500

    feat: list of movies in trend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log(`Api essss: ${API_KEY}`);
+const getTrendingPreview = async () => {
+	const res = await fetch(
+		`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+	);
+	const data = await res.json();
+
+	const movies = data.results;
+
+	movies.forEach((movie) => {
+		console.log(movie);
+		const trendindPreviewMoviesContainer = document.querySelector(
+			"#trendingPreview .trendingPreview-movieList"
+		);
+
+		const movieContainer = document.createElement("div");
+		movieContainer.classList.add("movie-container");
+
+		const movieImg = document.createElement("img");
+		movieImg.classList.add("movie-img");
+		movieImg.setAttribute("alt", movie.title);
+		movieImg.setAttribute(
+			"src",
+			"https://image.tmdb.org/t/p/w300" + movie.poster_path
+		);
+		movieContainer.appendChild(movieImg);
+		trendindPreviewMoviesContainer.appendChild(movieContainer);
+	});
+};
+
+getTrendingPreview();
